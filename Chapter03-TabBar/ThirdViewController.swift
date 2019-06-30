45b107f97b6c860c0e89a0052a37db43fc3b3229
Terry <terry.ssumu@gmail.com>
2019-06-30 21:30:57 +0900

    Initial Commit

---
+++ Chapter03-TabBar/ThirdViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ThirdViewController.swift
+//  Chapter03-TabBar
+//
+//  Created by TaeinKim on 30/06/2019.
+//  Copyright © 2019 TaeinKim. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "세 번째 탭"
+        title.textColor = UIColor.red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        title.sizeToFit()
+        title.center.x = self.view.frame.width / 2
+        
+        self.view.addSubview(title)
+    }
+}
